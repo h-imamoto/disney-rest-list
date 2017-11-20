bfe5e1d921d86b2031b759206caea40a5a07dfb5
m-ohashi <m-ohashi@biglobe.co.jp>
2017-11-20 14:16:16 +0900

    生データから生成されるクラスを作成

---
+++ src/app/original-land.ts
@@ -0,0 +1,7 @@
+import { OriginalAtraction } from './original-atraction';
+import { OriginalShow } from './original-show';
+
+export class OriginalLand {
+    atraction: OriginalAtraction;
+    show: OriginalShow;
+}
---
+++ src/app/original-sea.ts
@@ -0,0 +1,7 @@
+import { OriginalAtraction } from './original-atraction';
+import { OriginalShow } from './original-show';
+
+export class OriginalSea {
+    atraction: OriginalAtraction;
+    show: OriginalShow;
+}
