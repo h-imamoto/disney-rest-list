b1cccd90c91356d9c554c6cbc35e9cf36e8d0b91
m-ohashi <m-ohashi@biglobe.co.jp>
2017-12-04 13:50:32 +0900

    休止系のクラスにコンストラクタを追加

--- src/app/resting-atraction.ts
+++ src/app/resting-atraction.ts
@@ -2,4 +2,8 @@ import { DateSetAtraction } from './date-set-atraction';
 
 export class RestingAtraction implements DateSetAtraction {
     name: String;
+
+    constructor(name: String) {
+        this.name = name;
+    }
 }
--- src/app/resting-show.ts
+++ src/app/resting-show.ts
@@ -2,4 +2,8 @@ import { DateSetShow } from './date-set-show';
 
 export class RestingShow implements DateSetShow {
     name: String;
+
+    constructor(name: String) {
+        this.name = name;
+    }
 }
