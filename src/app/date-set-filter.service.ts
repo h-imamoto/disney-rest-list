1d6231560bd9ad205526b65e49e6e041d389d750
m-miyano-biglobe <m-miyano@za.biglobe.co.jp>
2017-12-04 13:47:40 +0900

    ピンポイント日付検索時のテストデータ追加

--- src/app/date-set-filter.service.ts
+++ src/app/date-set-filter.service.ts
@@ -1,6 +1,13 @@
 import { Injectable } from '@angular/core';
 
-import  {DateSetPark} from './date-set-park.ts;
+import  {DateSetPark} from './date-set-park';
+import { DateSetAtraction } from "./date-set-atraction";
+import { RestTerm} from "./rest-term"
+import { EndDate } from "./end-date"
+import { DisneyDate } from "./disney-date"
+import { DateSetShow } from "./date-set-show";
+import { DateSetLand } from "./date-set-land";
+import { DateSetSea } from "./date-set-sea";
 
 @Injectable()
 export class DateSetFilterService {
@@ -8,6 +15,43 @@ export class DateSetFilterService {
   constructor() { }
 
   get_schedule(time_now) : DateSetPark {
-    return new DateSetPark()
+    return new DateSetPark(
+      new DateSetLand(
+        [
+          new DateSetAtraction('テストアトラクション1'),
+          new DateSetAtraction('テストアトラクション2'),
+          new DateSetAtraction('テストアトラクション3'),
+          new DateSetAtraction('テストアトラクション4'),
+          new DateSetAtraction('テストアトラクション5'),
+          new DateSetAtraction('テストアトラクション6'),
+        ],
+        [
+          new DateSetShow('テストショウ1'),
+          new DateSetShow('テストショウ2'),
+          new DateSetShow('テストショウ3'),
+          new DateSetShow('テストショウ4'),
+          new DateSetShow('テストショウ5'),
+          new DateSetShow('テストショウ6'),
+        ]
+      ),
+      new DateSetSea(
+        [
+          new DateSetAtraction('テストアトラクション1'),
+          new DateSetAtraction('テストアトラクション2'),
+          new DateSetAtraction('テストアトラクション3'),
+          new DateSetAtraction('テストアトラクション4'),
+          new DateSetAtraction('テストアトラクション5'),
+          new DateSetAtraction('テストアトラクション6'),
+        ],
+        [
+          new DateSetShow('テストショウ1'),
+          new DateSetShow('テストショウ2'),
+          new DateSetShow('テストショウ3'),
+          new DateSetShow('テストショウ4'),
+          new DateSetShow('テストショウ5'),
+          new DateSetShow('テストショウ6'),
+        ]
+      )
+    )
   }
 }
