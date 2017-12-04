ee2e5df85e72db413de074ca7b75060899378e3d
m-ohashi <m-ohashi@biglobe.co.jp>
2017-12-04 13:20:22 +0900

    コンストラクタを作成

--- src/app/date-set-land.ts
+++ src/app/date-set-land.ts
@@ -4,4 +4,12 @@ import { RestingShow } from './resting-show';
 export class DateSetLand {
     atractionList: RestingAtraction[];
     showList: RestingShow[];
+
+    constructor(
+        atractionList: RestingAtraction[],
+        showList: RestingShow[]
+    ) {
+        this.atractionList = atractionList;
+        this.showList = showList;
+    }
 }
--- src/app/date-set-park.ts
+++ src/app/date-set-park.ts
@@ -5,4 +5,12 @@ import { DateSetSea } from './date-set-sea';
 export class DateSetPark implements Park {
     land: DateSetLand;
     sea: DateSetSea;
+
+    constructor(
+        land: DateSetLand,
+        sea: DateSetSea
+    ) {
+        this.land = land;
+        this.sea = sea;
+    }
 }
