5fd406ec8b6406dc6b96bd2811d209f96e1df6c3
imamoto <h-imamoto@za.biglobe.co.jp>
2017-12-07 14:06:51 +0900

    Fix DisneyService

--- src/app/disney.service.ts
+++ src/app/disney.service.ts
@@ -3,11 +3,9 @@ import { Injectable } from '@angular/core';
 import  {DateSetFilter} from './date-set-filter.service';
 import  {TermSetFilter} from './term-set-filter.service';
 import  {DisneyRepository} from './disney-repository.service';
-import { DisneyDate } from "./disney-date";
+import { DisneyDate } from './disney-date';
 
-@Injectable({
-  providers: [TermSetFilter, DateSetFilter]
-})
+@Injectable()
 export class DisneyService {
   constructor(private termSetFildter: TermSetFilter, private dateSetFilter: DateSetFilter) {}
 
@@ -15,7 +13,7 @@ export class DisneyService {
     this.dateSetFilter.get_schedule(time_now)
   }
 
-  get_term_filtered_schedule(startDate: DisneyDate, term){
-    this.termSetFildter.get_schedule(time_now, term)
+  get_term_filtered_schedule(startDate: DisneyDate, endDate: DisneyDate){
+    this.termSetFildter.get_schedule(startDate, endDate)
   }
 }
--- src/app/term-set/term-set.component.ts
+++ src/app/term-set/term-set.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { DisneyService } from '../disney.service';
+import { DateSetFilter } from '../date-set-filter.service';
 
 @Component({
   selector: 'app-term-set',
   templateUrl: './term-set.component.html',
   styleUrls: ['./term-set.component.css'],
   encapsulation: ViewEncapsulation.None,
-  providers: [DisneyService]
+  providers: [DisneyService, DateSetFilter]
 })
 export class TermSetComponent implements OnInit {
 
