81868b5eef3e34e0209109f0419db7621912d8e0
月岡祐太 <y-tsukioka@biglobe.co.jp>
2017-12-11 13:57:04 +0900

    get_original_parkのIF

--- src/app/disney-repository.service.ts
+++ src/app/disney-repository.service.ts
@@ -5,4 +5,8 @@ import { OriginalPark } from './original-park';
 @Injectable()
 export class DisneyRepository {
   constructor() {}
+
+  get_original_park(): OriginalPark {
+    return new OriginalPark();
+  }
 }
