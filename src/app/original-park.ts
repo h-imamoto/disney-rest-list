e3bbe276a4106ad3facb4c7a6b0cb4d0b5122356
m-miyano-biglobe <m-miyano@za.biglobe.co.jp>
2017-12-20 16:43:46 +0900

    Original側クラスを修正

--- src/app/original-park.ts
+++ src/app/original-park.ts
@@ -2,6 +2,11 @@ import { OriginalLand } from './original-land';
 import { OriginalSea } from './original-sea';
 
 export class OriginalPark {
-    originalLand: OriginalLand;
-    originalSea: OriginalSea;
+  originalLand: OriginalLand;
+  originalSea: OriginalSea;
+
+  //constructor(originalLand: OriginalLand, originalSea: OriginalSea) {
+  //  this.originalLand = originalLand;
+  //  this.originalSea = originalSea;
+  //}
 }
