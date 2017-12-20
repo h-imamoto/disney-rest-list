665c8b130c198a2faf8e30bbcbfb84d2a7a53b75
m-ohashi <m-ohashi@biglobe.co.jp>
2017-12-20 20:55:58 +0900

    期間指定判定のメソッドを作成

--- src/app/rest-term.ts
+++ src/app/rest-term.ts
@@ -1,4 +1,5 @@
 import { DisneyDate } from './disney-date';
+import { start } from 'repl';
 
 export class RestTerm {
     restStartDate: DisneyDate;
@@ -18,14 +19,37 @@ export class RestTerm {
     }
 
     public isRest(targetDate: DisneyDate): boolean {
-        return (targetDate.isAfter(this.restStartDate)
-            && targetDate.isBefore(this.restEndDate))
-            || targetDate.isEqual(this.restStartDate)
-            || targetDate.isEqual(this.restEndDate);
+        return (targetDate.isAfter(this.restStartDate) || targetDate.isEqual(this.restStartDate))
+            && (targetDate.isBefore(this.restEndDate) || targetDate.isEqual(this.restEndDate));
     }
 
     public isRestTerm(targetStarDate: DisneyDate, targetEndDate: DisneyDate): boolean {
-        return this.isRest(targetStarDate) || this.isRest(targetEndDate);
+        return this.isPinpoint(targetStarDate, targetEndDate)
+            || this.isExistRestTerm(targetStarDate, targetEndDate)
+            || this.isRestTermBefore(targetStarDate, targetEndDate)
+            || this.isRestTermAfter(targetStarDate, targetEndDate);
+    }
+
+    private isPinpoint(targetStarDate: DisneyDate, targetEndDate: DisneyDate): boolean {
+        return (this.restStartDate.isEqual(this.restEndDate))
+            && this.isExistRestTerm(targetStarDate, targetEndDate);
+    }
+
+    private isExistRestTerm(targetStarDate: DisneyDate, targetEndDate: DisneyDate): boolean {
+        return (targetStarDate.isEqual(this.restStartDate) || targetStarDate.isBefore(this.restStartDate))
+            && (targetEndDate.isEqual(this.restEndDate) || targetEndDate.isAfter(this.restEndDate));
+    }
+
+    private isRestTermBefore(targetStarDate: DisneyDate, targetEndDate: DisneyDate): boolean {
+        return ((targetStarDate.isAfter(this.restStartDate) && targetStarDate.isBefore(this.restEndDate))
+            && (targetEndDate.isEqual(this.restEndDate) || targetEndDate.isAfter(this.restEndDate)))
+            || targetStarDate.isEqual(this.restEndDate);
+    }
+
+    private isRestTermAfter(targetStarDate: DisneyDate, targetEndDate: DisneyDate): boolean {
+        return ((targetStarDate.isBefore(this.restStartDate))
+            && (targetEndDate.isBefore(this.restEndDate) && targetEndDate.isAfter(this.restStartDate)))
+            || targetEndDate.isEqual(this.restStartDate);
     }
 
 
